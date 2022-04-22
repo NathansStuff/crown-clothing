ec5b5e2752daaade6629ea78259aa8660153ca99
NathansStuff <n.odonnell@hotmail.com>
2022-04-22 16:37:50 +1000

    redux setup

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Fragment, useContext } from 'react';
 
 import {
@@ -10,12 +10,13 @@ import {
 import { ReactComponent as CrownLogo } from '../../../assets/crown.svg';
 import CartIcon from '../../cart-icon/cart-icon.component';
 import CartDropdown from '../../cart-dropdown/cart-dropdown.component';
-import { UserContext } from '../../../contexts/user.contexts';
 import { CartContext } from '../../../contexts/cart.context';
 import { signOutUser } from '../../../utils/firebase/firebase.utils';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../../store/user/user.selector';
 
 export default function Navigation() {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
 
   return (
