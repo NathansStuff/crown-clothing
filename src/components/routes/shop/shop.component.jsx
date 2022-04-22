3fa2f87557548bbc1171331b71bec97148dad418
NathansStuff <n.odonnell@hotmail.com>
2022-04-22 12:13:55 +1000

    shop nested route

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,20 +1,11 @@
-import './shop.styles.scss';
-
-import { useContext } from 'react';
-import { CategoriesContext } from '../../../contexts/categories.context';
-import CategoryPreview from '../../category-preview/category-preview';
+import { Routes, Route } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categoriess-preview.component';
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <div className='shop-container'>
-      {Object.keys(categoriesMap).map(title => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 
